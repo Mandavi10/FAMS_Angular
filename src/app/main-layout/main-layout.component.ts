06cef59c0dc60ae6c554cd3c1119533a3f38ffcf
Mandavi10 <mandavi@yoekisoft.com>
2020-05-28 18:19:22 +0530

    Home

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router,ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })
 export class MainLayoutComponent implements OnInit {
-
-  constructor() { }
+  showSideNav = true;
+  constructor(router: Router) {
+    router.events.subscribe(event => {
+      if (router.url === '/Home') {
+        this.showSideNav = false;
+      }
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   public shownav = false;
   isShow = false;
+ 
   constructor() { }
   
   ngOnInit() {
