61d659d3129218764db7d795b05e71953d80d61d
TARUN SHARMA <rtdigitalsolution@gmail.com>
2020-05-28 02:30:16 -0700

    changes

--- src/app/master/state-master/state-master.component.ts
+++ src/app/master/state-master/state-master.component.ts
@@ -7,16 +7,21 @@ import { AgGridAngular } from 'ag-grid-angular';
 })
 export class StateMasterComponent implements OnInit {
   columnDefs = [
-    {headerName: 'Sr. No.', field: 'srNo', sort: 'asc' },
-    {headerName: 'State Code', field: 'stateCode', sort: 'asc' },
-    {headerName: 'State Name', field: 'stateName', sort: 'asc'}
-   
+ 
+    {headerName: 'Sr. No.', field: 'srNo', width:'80'},
+    {headerName: 'Country  Code', field: 'countrycode', width:'150'},
+    {headerName: 'Country Name', field: 'countryname', width:'150'},
+    {headerName: 'Created By', field: 'createdby', width:'150'},
+    {headerName: 'Created On', field: 'createdon', width:'150'},
+    {headerName: 'Updated By', field: 'updatedby', width:'150'},
+    {headerName: 'Updated On', field: 'updatedon', width:'150'},
+    
 ];
 
 rowData = [
-    { srNo: '1', stateCode: 'Code-001', stateName: 'State 1'},
-    { srNo: '2', stateCode: 'Code-002', stateName: 'State 2'},
-    { srNo: '3', stateCode: 'Code-003', stateName: 'State 3'}
+    {  srNo: '1', countrycode:'IND',  countryname: 'India', createdby: 'Tarun', createdon:'28/05/2020',  updatedby: 'Tarun', updatedon:'28/05/2020'},
+    {  srNo: '2', countrycode:'AUS',  countryname: 'Australia' , createdby: 'Tarun', createdon:'28/05/2020',  updatedby: 'Tarun', updatedon:'28/05/2020'},
+    {  srNo: '3', countrycode:'PAK',  countryname: 'Pakistan' , createdby: 'Tarun', createdon:'28/05/2020',  updatedby: 'Tarun', updatedon:'28/05/2020'}
 ];
 
 
