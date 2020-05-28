b619915c47f77d99da5cd53886ed2acf2a80b12a
Mandavi10 <mandavi@yoekisoft.com>
2020-05-28 20:19:43 +0530

    Securities

--- src/app/master/securities-master/securities-master.module.ts
+++ src/app/master/securities-master/securities-master.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecuritiesMasterComponent } from './securities-master.component';
 import { SecuritiesMasterRoutingModule } from './securities-master-routing.module';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [SecuritiesMasterComponent],
   imports: [
     CommonModule,
-    SecuritiesMasterRoutingModule
+    SecuritiesMasterRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class SecuritiesMasterModule { }
