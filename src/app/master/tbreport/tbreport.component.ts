65f8225775067c754b7b5e906227f554a997d7b0
Mandavi10 <mandavi@yoekisoft.com>
2020-05-29 18:54:24 +0530

    TBReport

--- src/app/master/tbreport/tbreport.component.ts
+++ src/app/master/tbreport/tbreport.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TBReportComponent implements OnInit {
 
+  columnDefs = [
+    {headerName: 'All', field: 'all', width:'60', cellRenderer: function(){
+      return'<input type="checkbox" class="texBox" value="All" style="width:15px"/>'
+          }},
+    {headerName: 'Sr. No.', field: 'srNo', width:'80'},
+    {headerName: 'Customer Name', field: 'customername', width:'150'},
+    {headerName: 'Accounts', field: 'accounts', width:'150'},
+    {headerName: 'Account Sub Level', field: 'accountsublevel', width:'150'},
+    {headerName: 'From date', field: 'fromdate', width:'150'},
+    {headerName: 'To Date', field: 'todate', width:'150'},
+    {headerName: 'Account Type', field: 'accounttype', width:'150'},
+    
+    
+];
+
+rowData = [
+    {  srNo: '1', customername:'Tarun', accounts: '1222983474602', accountsublevel:'Sub Level 1', fromdate:'29/05/2020',todate:'29/05/2020', accounttype:'Account Type 1'},
+    {  srNo: '2', customername:'Tarun', accounts: '1222983474602', accountsublevel:'Sub Level 1', fromdate:'29/05/2020',todate:'29/05/2020' , accounttype:'Account Type 2'},
+    {  srNo: '3', customername:'Tarun', accounts: '1222983474602', accountsublevel:'Sub Level 1', fromdate:'29/05/2020',todate:'29/05/2020' , accounttype:'Account Type 3'},
+
+   
+];
+
+
+
+
+  showModalstatemaster: boolean;
+
+  onClickstatemaster(event) {
+    this.showModalstatemaster = true;
+    
+    }
+    
+    hidestatemaster() {
+    this.showModalstatemaster = false;
+    }
+
+
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/master/tbreport/tbreport.module.ts
+++ src/app/master/tbreport/tbreport.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TBReportComponent } from './tbreport.component';
 import { TBReportRoutingModule } from './tbreport-routing.module';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [TBReportComponent],
   imports: [
     CommonModule,
-    TBReportRoutingModule
+    TBReportRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class TBReportModule { }
