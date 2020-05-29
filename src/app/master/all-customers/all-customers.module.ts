6994a460dfa4724a9fbb31c6d9a90d044d4016d5
abhishek <abhishekvats367@gmail.com>
2020-05-29 13:50:00 +0530

    all customer

--- src/app/master/all-customers/all-customers.module.ts
+++ src/app/master/all-customers/all-customers.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllCustomersComponent } from './all-customers.component';
 import { AllCustomersRoutingModule } from './all-customers-routing.module';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [AllCustomersComponent],
   imports: [
     CommonModule,
-    AllCustomersRoutingModule
+    AllCustomersRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class AllCustomersModule { }
