63f3e8dba8b3ca0775739ca12621a19fdbde990e
abhishek <abhishekvats367@gmail.com>
2020-05-30 12:15:23 +0530

    cash equivalent and transaction statement

--- src/app/master/transaction-statement/transaction-statement.module.ts
+++ src/app/master/transaction-statement/transaction-statement.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionStatementComponent } from './transaction-statement.component';
 import { TransactionStatementRoutingModule } from './transaction-statement-routing.module';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [TransactionStatementComponent],
   imports: [
     CommonModule,
-    TransactionStatementRoutingModule
+    TransactionStatementRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class TransactionStatementModule { }
