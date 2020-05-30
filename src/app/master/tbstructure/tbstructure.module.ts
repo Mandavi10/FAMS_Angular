a9c04762c1408c762992dc2fbb9ba04df0c41ed1
Mandavi10 <mandavi@yoekisoft.com>
2020-05-30 19:03:10 +0530

    TB Structure

--- src/app/master/tbstructure/tbstructure.module.ts
+++ src/app/master/tbstructure/tbstructure.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TBstructureComponent } from './tbstructure.component';
 import { TBstructureRoutingModule } from './tbstructure-routing.module';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [TBstructureComponent],
   imports: [
     CommonModule,
-    TBstructureRoutingModule
+    TBstructureRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class TBstructureModule { }
