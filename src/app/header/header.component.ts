5f37b677f4152c70a3c5844c960dfa22cff15b20
Rock <kisuvisu95@gmail.com>
2020-06-03 03:31:22 -0700

    work in Access right

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import{DbsecurityService}from '../Services/dbsecurity.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  UserName : any; UserId : any;
   public shownav = false;
   isShow = false;
   isShow1 = false;
@@ -15,9 +17,11 @@ export class HeaderComponent implements OnInit {
   toggleCorporate() {
     this.isShow1 = !this.isShow1;
 }
-  constructor(private router: Router) { }
+  constructor(private router: Router,private Dbsecurity: DbsecurityService) { }
 
   ngOnInit() {
+    let Sessionvalue = JSON.parse(sessionStorage.getItem('User'));
+    this.UserName = this.Dbsecurity.Decrypt(Sessionvalue.UserName);
   }
   togglenav() {
     this.shownav = !this.shownav;
@@ -25,5 +29,22 @@ export class HeaderComponent implements OnInit {
   logout() {
     sessionStorage.clear();
     this.router.navigate(['/Login']);
+}
+HomeFun(){
+  let Sessionvalue = JSON.parse(sessionStorage.getItem('User'));
+  this.UserId = this.Dbsecurity.Decrypt(Sessionvalue.UserId);
+  if(this.UserId == "5")
+                  {                 
+                    this.router.navigate(['/Dashboard']);
+                  }
+                  else if(this.UserId  == "3"
+                  || this.UserId  == "1"
+                  || this.UserId  == "2")
+                  {
+                    this.router.navigate(['/Home']);
+                  }
+                else{
+                    this.router.navigate(['/Dashboard']);
+                  }
 }
 }
