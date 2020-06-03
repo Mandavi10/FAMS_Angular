564fc4b5ebbfa72bc9bf1b1015a9fa966aa3222b
abhishek <abhishekvats367@gmail.com>
2020-06-03 10:38:53 +0530

    popup

--- src/app/master/user-setup/user-setup.component.ts
+++ src/app/master/user-setup/user-setup.component.ts
@@ -10,6 +10,7 @@ import { MultiSelectService } from '../../multi-select.service';
 export class UserSetupComponent implements OnInit {
   Isdiv1:boolean;
   Isdiv2:boolean;
+  showModalsavepopup: boolean;
   columnDefs = [
     {headerName: 'Sr. No.', field: 'srNo', width: 80},
     {headerName: 'User Name', field: 'UserName', width: 150},
@@ -27,7 +28,14 @@ rowData = [
 ];
 
 
-
+onClicksavepopup(event) {
+  this.showModalsavepopup = true;
+  
+  }
+  
+  hidesavepopup() {
+  this.showModalsavepopup = false;
+  }
 
 
 
