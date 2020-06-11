a99ca15e5bc0c32eea049ba1c48e2643f9a4be62
Rock <kisuvisu95@gmail.com>
2020-06-10 23:34:34 -0700

    work on BrokerMaster

---
+++ src/Models/BrokerMaster/jsondata.ts
@@ -0,0 +1,15 @@
+export class Jsondata {
+    UserId : string;
+    BrokerName : string; 
+    TradeName : string;
+    RegistrationNo :  string; 
+    GSTNo :  string; 
+    StockExchangeName : string;
+    Email :string; 
+    Telephone : string; 
+    ContactEmail : string; 
+    Phone : string; 
+    Extension : string; 
+    MobileNo :string;
+    ContactPerson : string;
+}
--- src/app/master/broker-master/broker-master.module.ts
+++ src/app/master/broker-master/broker-master.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BrokerMasterComponent } from './broker-master.component';
 import { BrokerMasterRoutingModule } from './broker-master-routing.module';
 import { AgGridModule } from 'ag-grid-angular';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [BrokerMasterComponent],
   imports: [
     CommonModule,
-    BrokerMasterRoutingModule,
+    BrokerMasterRoutingModule,FormsModule,ReactiveFormsModule,
     AgGridModule.withComponents([])
   ]
 })
