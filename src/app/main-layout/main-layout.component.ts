e0ad5c28d1802730d7271c4754ca9dda61ab7d2e
bibhuyoeki <bibhu@yoekisoft.com>
2020-06-15 12:28:09 +0530

    ChangesSideBar_15June_Bibhu

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -11,26 +11,26 @@ export class MainLayoutComponent implements OnInit {
   constructor(router: Router) {
     router.events.subscribe(event => {
       if (router.url === '/Home') {
-        this.showSideNav = false;
+        this.showSideNav = true;
       }
-      else if (router.url === '/CashEquivalent') {
-        this.showSideNav = false;
-      }
-      else if (router.url === '/HoldingReport') {
-        this.showSideNav = false;
-      }
-      else if (router.url === '/TrialBalanceReport') {
-        this.showSideNav = false;
-      }
-      else if (router.url === '/SummaryReport') {
-        this.showSideNav = false;
-      }
-      else if (router.url === '/TransactionStatement') {
-        this.showSideNav = false;
-      }
-      else if (router.url === '/OrderProcessingNew') {
-        this.showSideNav = false;
-      }
+      // else if (router.url === '/CashEquivalent') {
+      //   this.showSideNav = false;
+      // }
+      // else if (router.url === '/HoldingReport') {
+      //   this.showSideNav = false;
+      // }
+      // else if (router.url === '/TrialBalanceReport') {
+      //   this.showSideNav = false;
+      // }
+      // else if (router.url === '/SummaryReport') {
+      //   this.showSideNav = false;
+      // }
+      // else if (router.url === '/TransactionStatement') {
+      //   this.showSideNav = false;
+      // }
+      // else if (router.url === '/OrderProcessingNew') {
+      //   this.showSideNav = false;
+      // }
     });
   }
 
