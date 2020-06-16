7063053230ad7abc68426838c3dc7fcf683669c6
Mandavi10 <mandavi@yoekisoft.com>
2020-06-16 11:48:56 +0530

    distributor master

---
+++ src/app/master/distributor-master/distributor-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-master',
+  templateUrl: './distributor-master.component.html',
+  styleUrls: ['./distributor-master.component.css']
+})
+export class DistributorMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
