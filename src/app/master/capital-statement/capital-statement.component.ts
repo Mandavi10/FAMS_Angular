89465a5de64ab06a50ebebf3a785ca1a10bcb5b5
Rock <kisuvisu95@gmail.com>
2020-07-29 21:32:25 -0700

    rishab code

--- src/app/master/capital-statement/capital-statement.component.ts
+++ src/app/master/capital-statement/capital-statement.component.ts
@@ -1143,20 +1143,20 @@ this.StaticArray2={value:"",value1:"Sale",value2:"",value3:"",value4:"",value5:"
     //   doc.save('StatementOfExpenses_Summary.pdf');
     // }
   
-    // var data = document.getElementById('gridmain');  
-    //   html2canvas(data).then(canvas => {  
-    //     // Few necessary setting options  
-    //     var imgWidth = 208;   
-    //     var pageHeight = 295;    
-    //     var imgHeight = canvas.height * imgWidth / canvas.width;  
-    //     var heightLeft = imgHeight;  
+    var data = document.getElementById('capitalStatementGrid');  
+      html2canvas(data).then(canvas => {  
+        // Few necessary setting options  
+        var imgWidth = 208;   
+        var pageHeight = 295;    
+        var imgHeight = canvas.height * imgWidth / canvas.width;  
+        var heightLeft = imgHeight;  
     
-    //     const contentDataURL = canvas.toDataURL('image/png')  
-    //     let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
-    //     var position = 0;  
-    //     pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-    //     pdf.save('StatementOfExpenses_Html.pdf'); // Generated PDF   
-    //   });    
+        const contentDataURL = canvas.toDataURL('image/png')  
+        let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+        var position = 0;  
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+        pdf.save('CapitalStatement Gain/Loss_Html.pdf'); // Generated PDF   
+      });    
     
   
   }
