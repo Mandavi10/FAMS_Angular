fb39219453065fa2ce1f552fd869adcc9b873635
Vipul Jain <vipulcrystal@gmail.com>
2020-08-04 12:07:09 +0530

    Portfolio Appraisal

---
+++ src/Models/PortfolioAppraisals/portfolio-appraisals.spec.ts
@@ -0,0 +1,7 @@
+import { PortfolioAppraisals } from './portfolio-appraisals';
+
+describe('PortfolioAppraisals', () => {
+  it('should create an instance', () => {
+    expect(new PortfolioAppraisals()).toBeTruthy();
+  });
+});
