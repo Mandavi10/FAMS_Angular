c938aaf1ec3c7f0407b7c496fbf6bec626b710c8
Rishabh <rishabh@yoekisoft.com>
2020-08-06 17:17:27 +0530

    rishabh changes

--- src/app/Services/PortfolioAppraisals/portfolio-appraisals.service.ts
+++ src/app/Services/PortfolioAppraisals/portfolio-appraisals.service.ts
@@ -13,8 +13,8 @@ export class PortfolioAppraisalsService {
   baseUrl: string = ""; UserId: string = ""; EntityId: string = "";
   constructor(private _http: HttpClient, @Inject('BASE_URL') myAppUrl: string) {
       //this.baseUrl = myAppUrl;
-  //this.baseUrl = "http://localhost:55073/";
-  this.baseUrl = AppSettings.Login_URL;
+  this.baseUrl = "http://localhost:55073/";
+  // this.baseUrl = AppSettings.Login_URL;
   }
 
 
@@ -42,14 +42,20 @@ export class PortfolioAppraisalsService {
   }
   BindGridAllFields(em): Observable<any> {
     const body = em; 
-    alert('bind')
+    
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post<any>(this.baseUrl + 'api/PortfolioAppraisal/BindGrid/', body, {
+    return this._http.post<any>(this.baseUrl + 'api/PortfolioAppraisals/BindGrid/', body, {
       headers 
   });
   }
-  BindDefaultData(GAccountNumber,UserId): Observable<any> {
-    return this._http.get<any>(this.baseUrl + 'api/PortfolioAppraisals/BindDefaultData/' +GAccountNumber+ '/' +UserId   );
+  BindDefaultData(em): Observable<any> {
+    const body = em; 
+     
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.post<any>(this.baseUrl + 'api/PortfolioAppraisals/BindDefaultData/', body, {
+      headers 
+  });
+   // return this._http.get<any>(this.baseUrl + 'api/PortfolioAppraisals/BindDefaultData/' +GAccountNumber+ '/' +UserId   );
   }
   NextRecordBind(CustomerAccount,FromDate,ToDate,SeqNo): Observable<any> {
     return this._http.get<any>(this.baseUrl + 'api/PortfolioAppraisals/NextRecordBind/'+CustomerAccount+ '/' +FromDate +'/' + ToDate +'/' + SeqNo );
