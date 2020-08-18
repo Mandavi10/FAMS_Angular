87750a99d57d706d3507ca477f631abae44a69d3
Rock <kisuvisu95@gmail.com>
2020-08-18 04:40:46 -0700

    changes 2

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -2,14 +2,14 @@ export class AppSettings {
     // public static API_ENDPOINT='http://219.90.65.215:9003/';
 
 
-  public static API_ENDPOINT1='http://219.90.65.215:9010/';
+  //public static API_ENDPOINT1='http://219.90.65.215:9010/';
 
 
    //public static Login_URL='http://localhost:55073/';
 
 
 
-   //public static Login_URL='http://219.90.65.215:9003/';
+   public static Login_URL='http://219.90.65.215:9003/';
 
    
 
